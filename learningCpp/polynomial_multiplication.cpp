2c98ba50075fd3bb9284370b6d8aa0fabbf74a64
Anon0nyx <anon0nyx@Anon0nyxs-MacBook-Pro.local>
2024-10-19 02:13:12 -0400

    Trying to push from macbook

--- learningCpp/polynomial_multiplication.cpp
+++ learningCpp/polynomial_multiplication.cpp
@@ -2,30 +2,46 @@
 
 using namespace std;
 
-int main() {
-  int polyA[3] = {1,3,2};
-  int polyB[3] = {2,4,1};
-  int sumArr[5];
-  int termValue;
+int main() { // For this we start with the constant in the polynomial
+  int polyA[3] = {1,3,2}; // 1 + 3x + 2x^2
+  int polyB[3] = {2,4,1}; // 2 + 4x + x^2 
 
-  int terms = 4;
+	// Start by counting the size of our two coefficient arrays
+	int n = 0;
+	int m = 0;
+	for (int i : polyA) {
+		n++;
+	}
+	for (int j : polyB) {
+		m++;
+	} 
+
+	n--; // Remove our constant terms from the count
+	m--;
+
+	int termValue = 0;
+	int newValue = 0;
+	int terms = n + m;
+	int termsArr[terms];
   for (int k=0; k <= terms; k++) {
-    for (int j=0; j <= k; j++) {
-      cout << "Iteration: ";
-      cout << k;
-      cout << " Internal: ";
-      cout << j << endl;
-      termValue = 0;
+    termValue = 0; // Reset our term value as we shift in roots
+		for (int j=0; j <= k; j++) {
+      newValue = 0;
       if (!(k >= terms && j >= terms)) {
-        termValue = polyA[j] * polyB[k-j];
+        newValue = polyA[j] * polyB[k-j];
       }
       if ((j > 2) || (k == 4 && j >= terms) || (k-j > 2)) {
-        termValue = 0;
+        newValue = 0;
       }
-
-      cout << termValue << endl;
+			termValue += newValue;
     }
+		termsArr[k] = termValue;
   }
+
+	for (int i=0; i<=terms; i++) {
+		cout << termsArr[i] << "x_" << i << " + ";
+	}
+	cout << endl;
 
   return 0;
 }
